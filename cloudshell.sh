feaef8f4e9c908e99d5474aecf1cae55c2d81331
simopunkc <pembela.allah@gmail.com>
2022-12-04 09:16:27 +0700

    update runtime to nodejs v14

--- cloudshell.sh
+++ cloudshell.sh
@@ -18,7 +18,7 @@ gcloud projects remove-iam-policy-binding $PROJECT_ID --member $LASTUSER --role 
 
 cd script-nodejs/
 
-gcloud functions deploy $CLOUD_FUNCTION_NAME --region=us-east1 --trigger-bucket=gs://$BUCKET_NAME --runtime=nodejs12 --entry-point=thumbnail  --quiet
+gcloud functions deploy $CLOUD_FUNCTION_NAME --region=us-east1 --trigger-bucket=gs://$BUCKET_NAME --runtime=nodejs14 --entry-point=thumbnail  --quiet
 
 cd ..
 gsutil cp map.jpg gs://$BUCKET_NAME/
