c26a9ce23b6245d39969fa2c92cf302e9862d3b5
simopunkc <pembela.allah@gmail.com>
2022-12-04 08:27:43 +0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # GSP315
+
+git clone https://github.com/simopunkc/GSP315.git
+cd GSP315
+chmod +x cloudshell.sh
+./cloudshell.sh
---
+++ cloudshell.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+gcloud config set compute/zone us-east1-b
+
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+gsutil mb -l us-east1 gs://$PROJECT_ID
+
+export BUCKET_NAME="memories-bucket-93202"
+export TOPIC_NAME="memories-topic-301"
+export CLOUD_FUNCTION_NAME="memories-thumbnail-generator"
+
+gcloud pubsub topics create $TOPIC_NAME
+
+export LASTUSER=$(gcloud projects get-iam-policy $BUCKET_NAME --flatten="bindings[].members" --format='table(bindings.members)' --filter="bindings.members:user:student*" |& tail -1)
+
+gcloud projects remove-iam-policy-binding $BUCKET_NAME --member $LASTUSER --role roles/viewer
+
+cd script-nodejs/
+
+gcloud functions deploy $CLOUD_FUNCTION_NAME --region=us-east1 --trigger-bucket=gs://$BUCKET_NAME --runtime=nodejs12 --entry-point=thumbnail  --quiet
+
+cd ..
+gsutil cp map.jpg gs://$BUCKET_NAME/
